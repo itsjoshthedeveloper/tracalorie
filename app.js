882a74d0e1b50d964b375118693fad2e581ca03b
Josh Chough <joshchough@gmail.com>
2020-06-09 22:49:00 -0400

    Got and populated items into list

--- app.js
+++ app.js
@@ -9,22 +9,53 @@ const ItemCtrl = (function () {
 
   // Data structure / state
   const data = {
-    items: [],
+    items: [{ id: 0, name: 'Boba', calories: 300 }],
     currentItem: null,
     totalCalories: 0,
+  };
+
+  return {
+    getItems: function () {
+      return data.items;
+    },
+    logData: function () {
+      console.log(data);
+    },
   };
 })();
 
 // UI Controller
 const UICtrl = (function () {
-  //
+  const UISelectors = {
+    itemList: document.querySelector('#item-list'),
+  };
+  return {
+    populateItemsList: function (items) {
+      let html = '';
+      items.forEach((item) => {
+        html += `
+          <li class="collection-item" id="item-${item.id}">
+            <strong>${item.name}: </strong><em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fa fa-pencil"></i>
+            </a>
+          </li>
+        `;
+      });
+      UISelectors.itemList.innerHTML = html;
+    },
+  };
 })();
 
 // App Controller
 const App = (function (ItemCtrl, UICtrl) {
   return {
     init: function () {
-      console.log('Initializing app...');
+      // Get items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate item list with items
+      UICtrl.populateItemsList(items);
     },
   };
 })(ItemCtrl, UICtrl);
