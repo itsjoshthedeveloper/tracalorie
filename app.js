2d6c0409720835e58f072f5b6b37b5d6ab4bd024
Josh Chough <joshchough@gmail.com>
2020-06-09 23:11:23 -0400

    Added item to data structure

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ const ItemCtrl = (function () {
     getItems: function () {
       return data.items;
     },
+    addItem: function (name, calories) {
+      // Create ID
+      let ID = 0;
+      if (data.items.length > 0) {
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      data.items.push(new Item(ID, name, calories));
+    },
     logData: function () {
       console.log(data);
     },
@@ -28,6 +38,9 @@ const ItemCtrl = (function () {
 const UICtrl = (function () {
   const UISelectors = {
     itemList: document.querySelector('#item-list'),
+    addBtn: document.querySelector('.add-btn'),
+    itemName: document.querySelector('#item-name'),
+    itemCalories: document.querySelector('#item-calories'),
   };
   return {
     populateItemsList: function (items) {
@@ -44,11 +57,39 @@ const UICtrl = (function () {
       });
       UISelectors.itemList.innerHTML = html;
     },
+    getItemInput: function () {
+      return {
+        name: UISelectors.itemName.value,
+        calories: parseInt(UISelectors.itemCalories.value),
+      };
+    },
+    getUISelectors: function () {
+      return UISelectors;
+    },
   };
 })();
 
 // App Controller
 const App = (function (ItemCtrl, UICtrl) {
+  // Load event listeners
+  const loadEventListeners = function () {
+    // Get UI selectors
+    const UISelectors = UICtrl.getUISelectors();
+    UISelectors.addBtn.addEventListener('click', addItem);
+  };
+
+  // Add item to data structure
+  const addItem = function (e) {
+    // Get item input
+    const input = UICtrl.getItemInput();
+    if (input.name !== '' && input.calories !== '') {
+      // Add item to data
+      ItemCtrl.addItem(input.name, input.calories);
+      ItemCtrl.logData();
+    }
+    e.preventDefault();
+  };
+
   return {
     init: function () {
       // Get items from data structure
@@ -56,6 +97,9 @@ const App = (function (ItemCtrl, UICtrl) {
 
       // Populate item list with items
       UICtrl.populateItemsList(items);
+
+      // Load listeners
+      loadEventListeners();
     },
   };
 })(ItemCtrl, UICtrl);
